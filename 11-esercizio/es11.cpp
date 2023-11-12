120395d772eadb6f2dad0a93ee4a2f9e3f8e0c2d
leo-sium <leonardozotti.d@gmail.com>
2023-11-12 19:01:39 +0100

    fine ese11

--- 11-esercizio/es11.cpp
+++ 11-esercizio/es11.cpp
@@ -11,8 +11,12 @@
 
 using namespace std;
 
-double f(double x){
-    return -log(x);
+double f_1(double x){
+    return -log(1-x);
+}
+
+double f_2(double x){
+    return sqrt(-log(0.5-x));
 }
 
 int main(){
@@ -21,20 +25,52 @@ int main(){
     double x = 0;
     double y = 0.;
     srand(time(0));
-    vector<double> punti;
+    vector<double> punti_1;
+    vector<double> punti_2;
+    vector<double> punti_3;
+
     for(int i=0; i<N; i++){
-        x = rand_range(exp(-2),1);
-        y = f(x);
-        punti.push_back(y);
+        x = rand_range(0,1-exp(-2));
+        y = f_1(x);
+        punti_1.push_back(y);
     }
 
-    fstream file;
-    file.open("punti.dat", ios_base::out);
-
-    for(int i=0; i<punti.size(); i++){
-        file << setprecision(10)<< punti[i] << endl;
+    for(int i=0; i<N; i++){
+        x = rand_range(1- exp(-1),1);
+        y = f_1(x);
+        punti_2.push_back(y);
     }
 
-    file.close();
+    for(int i=0; i<N; i++){
+        x = rand_range(0,1);
+        y = f_2(x);
+        punti_3.push_back(y);
+    }
+
+    fstream file_1;
+    file_1.open("punti_1.dat", ios_base::out);
+
+    for(int i=0; i<punti_1.size(); i++){
+        file_1 << setprecision(10)<< punti_1[i] << endl;
+    }
+    file_1.close();
+
+    fstream file_2;
+    file_2.open("punti_2.dat", ios_base::out);
+
+    for(int i=0; i<punti_2.size(); i++){
+        file_2 << setprecision(10)<< punti_2[i] << endl;
+    }
+    file_2.close();
+
+    fstream file_3;
+    file_3.open("punti_3.dat", ios_base::out);
+
+    for(int i=0; i<punti_3.size(); i++){
+        file_3 << setprecision(10)<< punti_3[i] << endl;
+    }
+    file_3.close();
+
+    return 0;
 
 }
--- 11-esercizio/plot.py
+++ 11-esercizio/plot.py
@@ -6,21 +6,61 @@ def f(x) :
     ris = np.exp(-x)
     return ris
 
-rawdata = np.loadtxt('punti.dat')
+def g(x) :
+    ris = x*np.exp(-np.power(x, 2))
+    return ris
 
-num_bins = 100
-h, bin_egdes, _ = plt.hist(rawdata, bins='auto', edgecolor='purple', color='purple', density=True)
+dati1 = np.loadtxt('punti_1.dat')
+dati2 = np.loadtxt('punti_2.dat')
+dati3 = np.loadtxt('punti_3.dat')
 
-x = np.linspace(min(bin_egdes), max(bin_egdes), 1000)
-y = f(x)
-a = np.trapz(y,x)
-y = y / a
+#primo istogramma ---------------------------------------------------------------------------------------
+h_1, bin_egdes_1, _ = plt.hist(dati1, bins='auto', edgecolor='purple', color='purple', density=True)
 
-plt.plot(x,y, 'r-', label='f')
-
+#sezione di creazione della funzione da sovrapporre
+x_1 = np.linspace(min(bin_egdes_1), max(bin_egdes_1), 1000)
+y_1 = f(x_1)
+a_1 = np.trapz(y_1,x_1)
+y_1 = y_1 / a_1
+#opzioni aggiuntive del grafico
+plt.plot(x_1,y_1, 'r-', label='f')
 plt.xlabel('valore')
-plt.ylabel('frequenza')
+plt.ylabel('densità')
 plt.title('dist exp')
+plt.legend()
+
+#secondo istogramma --------------------------------------------------------------------------------------
+plt.figure()
+h_2, bin_egdes_2, _ = plt.hist(dati2, bins='auto', edgecolor='purple', color='purple', density=True)
+
+#sezione di creazione della funzione da sovrapporre
+x_2 = np.linspace(min(bin_egdes_2), max(bin_egdes_2), 1000)
+y_2 = f(x_2)
+a_2 = np.trapz(y_2,x_2)
+y_2 = y_2 / a_2
+
+#opzioni aggiuntive del grafico
+plt.plot(x_2,y_2, 'r-', label='f')
+plt.xlabel('valore ')
+plt.ylabel('densità')
+plt.title('dist exp')
+plt.legend()
+
+#terzo istogramma ----------------------------------------------------------------------------------------
+plt.figure()
+h_3, bin_egdes_3, _ = plt.hist(dati3, bins='auto', edgecolor='purple', color='purple', density=True)
+
+#sezione di creazione della funzione da sovrapporre
+x_3 = np.linspace(min(bin_egdes_3), max(bin_egdes_3), 1000)
+y_3 = g(x_3)
+a_3 = np.trapz(y_3,x_3)
+y_3 = y_3 / a_3
+
+#opzioni aggiuntive del grafico
+plt.plot(x_3,y_3, 'r-', label='g')
+plt.xlabel('valore ')
+plt.ylabel('densità')
+plt.title('dist xexp(-x^2)')
+plt.legend()
 
 plt.show()
-
