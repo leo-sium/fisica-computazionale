71a88ba8121bd8bf159ac9ff2cd79e79913ca045
leo-sium <leonardozotti.d@gmail.com>
2023-11-13 22:34:02 +0100

    continuo es 10

--- 10-esercizio/es10.cpp
+++ 10-esercizio/es10.cpp
@@ -4,8 +4,11 @@
 #include "cmath"
 #include "integrali.h"
 #include "string.h"
+#include <cmath>
 #include <cstdlib>
+#include <fstream>
 #include <math.h>
+#include <vector>
 #include "time.h"
 
 using namespace std;
@@ -28,41 +31,91 @@ double f_3(double x){
 int main (int argc, char** argv){
 
     double N = pow(10, 5);
+
+    double I_1 = 672.19323731283680927664797017826546492574500186733942065874221332;
+    double I_2 = 1480.4609508621402842169020454124156183872962099931681274803958396;
+    double I_3 = 169.09199252874577513825077822164630272078603901996717867845299026;
+
     double xmin_1 = 0;
     double xmax_1 = 5;
     double ymin_1 = 0;
     double ymax_1 = f_1(7);
-
-    double I_hm_1 = hit_or_miss(xmin_1, xmax_1, ymin_1, ymax_1, N, f_1);
-    double I_mc_1 = monte_carlo(xmin_1, xmax_1, N, f_1 );
-    double I_trap_1 = trapezio(xmin_1, xmax_1, N, f_1);
-
-    cout << "primo integrale:" << endl << "metodo monte carlo\t"<< I_mc_1 << endl <<"metodo hit or miss\t" << I_hm_1 << endl << "metodo trapezio\t\t" << I_trap_1 << endl;
-
+    double I_hm_1 = 0;
+    double I_mc_1 = 0;
+    vector<double> dev_hm_1;
+    vector<double> dev_mc_1;
 
     double xmin_2 = 3;
     double xmax_2 = 8;
     double ymin_2 = 0;
     double ymax_2 = f_2(8);
-
-    double I_hm_2 = hit_or_miss(xmin_2, xmax_2, ymin_2, ymax_2, N, f_2);
-    double I_mc_2 = monte_carlo(xmin_2, xmax_2, N, f_2 );
-    double I_trap_2 = trapezio(xmin_2, xmax_2, N, f_2);
-
-    cout << "secondo integrale:"<< endl << "metodo monte carlo \t"<< I_mc_2 << endl << "metodo hit or miss\t" << I_hm_2 << endl << "metodo trapezio\t\t" << I_trap_2 << endl;
+    double I_hm_2 = 0;
+    double I_mc_2 = 0;
+    vector<double> dev_hm_2;
+    vector<double> dev_mc_2;
 
     double xmin_3 = -1;
     double xmax_3 = 8;
     double ymin_3 = 0;
     double ymax_3 = f_3(8);
+    double I_hm_3 = 0;
+    double I_mc_3 = 0;
+    vector<double> dev_hm_3;
+    vector<double> dev_mc_3;
 
-    double I_hm_3 = hit_or_miss(xmin_3, xmax_3, ymin_3, ymax_3, N, f_3);
-    double I_mc_3 = monte_carlo(xmin_3, xmax_3, N, f_3 );
-    double I_trap_3 = trapezio(xmin_3, xmax_3, N, f_3);
+    for(int i=1000; i<N; i+=1000){
 
-    cout << "terzo integrale:" << endl << "metodo monte carlo\t"<< I_mc_3 << endl << "metodo hit or miss\t" << I_hm_3 << endl << "metodo trapezio\t\t" << I_trap_3 << endl;
+        I_hm_1 = abs( hit_or_miss(xmin_1, xmax_1, ymin_1, ymax_1, i, f_1) - I_1);
+        I_mc_1 = abs( monte_carlo(xmin_1, xmax_1, i, f_1 ) - I_1);
+        dev_hm_1.push_back(I_hm_1);
+        dev_mc_1.push_back(I_mc_1);
 
-    //urlo del sium
+
+        //double I_trap_1 = trapezio(xmin_1, xmax_1, N, f_1);
+        //cout << "primo integrale:" << endl << "metodo monte carlo\t"<< I_mc_1 << endl <<"metodo hit or miss\t" << I_hm_1 << endl << "metodo trapezio\t\t" << I_trap_1 << endl;
+
+        I_hm_2 = abs( hit_or_miss(xmin_2, xmax_2, ymin_2, ymax_2, i, f_2) - I_2);
+        I_mc_2 = abs( monte_carlo(xmin_2, xmax_2, i, f_2 ) - I_2);
+        dev_hm_2.push_back(I_hm_2);
+        dev_mc_2.push_back(I_mc_2);
+        //double I_trap_2 = trapezio(xmin_2, xmax_2, N, f_2);
+        //cout << "secondo integrale:"<< endl << "metodo monte carlo \t"<< I_mc_2 << endl << "metodo hit or miss\t" << I_hm_2 << endl << "metodo trapezio\t\t" << I_trap_2 << endl;
+
+        I_hm_3 = abs( hit_or_miss(xmin_3, xmax_3, ymin_3, ymax_3, i, f_3) - I_3);
+        I_mc_3 = abs(monte_carlo(xmin_3, xmax_3, i, f_3 ) - I_3);
+        dev_hm_3.push_back(I_hm_3);
+        dev_mc_3.push_back(I_mc_3);
+
+        //double I_trap_3 = trapezio(xmin_3, xmax_3, N, f_3);
+        //cout << "terzo integrale:" << endl << "metodo monte carlo\t"<< I_mc_3 << endl << "metodo hit or miss\t" << I_hm_3 << endl << "metodo trapezio\t\t" << I_trap_3 << endl;
+
+    }
+    int j=1000;
+    fstream file_1;
+    file_1.open("integrale_1.dat", ios_base::out);
+    for (int i=0; i< dev_hm_1.size(); i++){
+        file_1 << j << "\t" << setprecision(10)<< dev_hm_1[i] << "\t" << setprecision(10) << dev_mc_1[i] << endl;
+        j+=1000;
+    }
+    file_1.close();
+
+    j=1000;
+    fstream file_2;
+    file_2.open("integrale_2.dat", ios_base::out);
+    for (int i=0; i< dev_hm_2.size(); i++){
+        file_2 << j << "\t" << setprecision(10)<< dev_hm_2[i] << "\t" << setprecision(10) << dev_mc_2[i] << endl;
+        j+=1000;
+    }
+    file_2.close();
+
+    j=1000;
+    fstream file_3;
+    file_3.open("integrale_3.dat", ios_base::out);
+    for (int i=0; i< dev_hm_3.size(); i++){
+        file_3 << j << "\t" << setprecision(10)<< dev_hm_3[i] << "\t" << setprecision(10) << dev_mc_3[i] << endl;
+        j+=1000;
+    }
+    file_3.close();
 
     return 0;
 }
---
+++ 10-esercizio/plot.py
@@ -0,0 +1,44 @@
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+
+def f(x, a) :
+    return a/np.sqrt(x)
+
+rawdata = np.loadtxt("integrale_1.dat")
+
+x = rawdata[:, 0]
+y_hm = rawdata[:, 1]
+y_mc = rawdata[:, 2]
+
+a, covarianza_hm = curve_fit(f, x, y_hm)
+y_fit_hm = f(x, a)
+
+
+#x_f = np.linspace(1000, 99000)
+#y_f = f(x_f)
+#plt.plot(x_f, y_f, color='blue', label='f')
+
+
+plt.scatter(x, y_hm, marker='.', color= 'purple', s=5, label='hit or miss' )
+#plt.plot(x, y_hm, color= 'purple', label='hit or miss' )
+
+plt.plot(x, y_fit_hm, color='blue', label='f')
+
+plt.xlabel('N')
+plt.ylabel('deviazione dal valore vero')
+plt.legend()
+
+plt.figure()
+plt.scatter(x, y_mc, marker='.', color= 'red', s=5, label='monte carlo' )
+#plt.plot(x, y_mc, color= 'red',  label='monte carlo' )
+
+b, covarianza_mc = curve_fit(f, x, y_mc)
+y_fit_mc = f(x, a)
+plt.plot(x, y_fit_mc, color='blue', label='f')
+
+plt.xlabel('N')
+plt.ylabel('deviazione dal valore vero')
+plt.legend()
+
+plt.show()
