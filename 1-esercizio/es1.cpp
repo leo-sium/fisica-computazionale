a80f7738190ef6bb0b048e7d59b3b7f04123a2d7
leo_sium <leonardozotti.d@gmail.com>
2024-01-09 19:14:36 +0100

    sistemazione es 1

--- 1-esercizio/es1.cpp
+++ 1-esercizio/es1.cpp
@@ -9,13 +9,13 @@
 
 using namespace std;
 
-// funzione che calcola la sommatoria in float
+// Funzione che calcola la sommatoria con precisione singola
 float singlef (int i){
     float ris = 1/pow(static_cast<float>(i),2);
     return ris;
 }
 
-//funzione che calcola la sommatoria sul double
+// Funzione che calcola la sommatoria con precisione doppia
 double doublef (int i){
     float ris = 1/pow(static_cast<double>(i),2);
     return ris;
@@ -23,77 +23,65 @@ double doublef (int i){
 
 int main (int argc, char** argv){
     int N = 10000;
+
+    float sum_f = 0;
+    float temp_f;
     fstream file1;
-    file1.open("datif1.dat", ios_base::out);
+    file1.open("datif_crescente.dat", ios_base::out);
 
-    
-    float sumf = 0;
-    float partsumf;
-
-
-// calcolo la sommatoria in singola precisione sommando con N crescente e scrivendo un numero ogni 100
-// in questo modo la somma è più efficiente perchè sfrutta i numeri già calcolati per quelli successivi, senza far ripartire la somma da 0
+// Calcolo della sommatoria in singola precisione con N crescente
+// Al variare di N viene calcolata la somma in modo da utilizzare i valori di S(N-1) calcolati in precedenza e viene scritta su un file di testo la discrepanza con il valore atteso
     for(int i=1; i<N; i++){
-        partsumf = singlef(i);
-        sumf+= partsumf;
-      
-        if (i%100 == 0){
-        float diff = abs(sumf - pow(M_PI, 2)/6);
-
+        temp_f = singlef(i);
+        sum_f+= temp_f;
+        float diff = abs(sum_f - pow(M_PI, 2)/6);
         file1 << i << "\t" << setprecision(100) << diff << endl;
-        }
+
     }
     file1.close();
 
     fstream file2;
-    file2.open("datif2.dat", ios_base::out);
+    file2.open("datif_decrescente.dat", ios_base::out);
 
-
- // calcolo la sommatoria in singola precisione con N decrescente, scrivendo un numero ogni 100   
-    for(int i=1; i<N; i+=100){
-        sumf = 0;
-        partsumf = 0;
+// Calcolo la sommatoria in singola precisione con N decrescente   
+    for(int i=1; i<N; ++i){
+        sum_f = 0;
+        temp_f = 0;
         for (int j=N-i; j>0; j-- ){
-            partsumf = singlef(j);
-            sumf+= partsumf;
+            temp_f = singlef(j);
+            sum_f+= temp_f;
         }
-        float diff = abs(sumf - pow(M_PI, 2)/6);
+        float diff = abs(sum_f - pow(M_PI, 2)/6);
         file2 << N-i << "\t" << setprecision(100) << diff << endl;
     }
-
     file2.close();
 
     fstream file3;
-    file3.open("datid1.dat", ios_base::out);
+    file3.open("datid_crescente.dat", ios_base::out);
+    double sum_d = 0;
+    double temp_d = 0;
 
-    double sumd = 0;
-    double partsumd = 0;
-// calcolo la sommatoria in doppia precisione sommando con N crescente e scrivendo un numero ogni 100
-// in questo modo la somma è più efficiente perchè sfrutta i numeri già calcolati per quelli successivi, senza far ripartire la somma da 0
+// Calcolo della sommatoria in doppia precisione con N crescente
+// Al variare di N viene calcolata la somma in modo da utilizzare i valori di S(N-1) calcolati in precedenza e viene scritta su un file di testo la discrepanza con il valore atteso
     for(int i=1; i<N; i++){
-        partsumd = doublef(i);
-        sumd+= partsumd;
-
-        if(i%100 == 0){
-            double diff = abs(sumd - pow(M_PI,2)/6);
-
-            file3 << i << "\t" << setprecision(100) << diff << endl;
-        }
+        temp_d = doublef(i);
+        sum_d+= temp_d;
+        double diff = abs(sum_d - pow(M_PI,2)/6);
+        file3 << i << "\t" << setprecision(100) << diff << endl;
     }
-
     file3.close();
 
     fstream file4;
     file4.open("datid2.dat", ios_base::out);
  // calcolo la sommatoria in doppia precisione con N decrescente, scrivendo un numero ogni 100 
     for(int i=1; i<N; i+=100){
-        sumd = 0;
-        partsumd = 0;
+        sum_d = 0;
+        temp_d = 0;
         for (int j=N-i; j>0; j-- ){
-            partsumd = doublef(j);
-            sumd+= partsumd;
+            temp_d = doublef(j);
+            sum_d+= temp_d;
         }
-        double diff = abs(sumd - pow(M_PI, 2)/6);
+        double diff = abs(sum_d - pow(M_PI, 2)/6);
         file4 << N-i << "\t" << setprecision(100) << diff << endl;
     }
 
